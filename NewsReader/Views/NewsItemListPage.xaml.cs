a47d7a2fc79c63fe8ce5bba6bd6028f2c6f50bcb
Naresh <naresh.thandu@gmail.com>
2020-07-14 08:49:29 +0100

    add to repo

---
+++ NewsReader/Views/NewsItemListPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using NewsReader.ViewModels;
+using Xamarin.Forms;
+
+namespace NewsReader.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class NewsItemListPage : ContentPage
+    {
+
+        NewsItemListViewModel _viewModel;
+
+        public NewsItemListPage()
+        {
+            InitializeComponent();
+            _viewModel = new NewsItemListViewModel(Navigation);
+            BindingContext = _viewModel;
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            
+            await _viewModel.Init();
+        }
+        
+    }
+}
